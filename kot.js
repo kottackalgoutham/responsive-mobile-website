619a577c884591953cd6072f8b23a210ca82e3e9
ajithkottackal <kottackalajith@gmail.com>
2017-04-27 14:16:33 +0530

    first commit

---
+++ kot.js
@@ -0,0 +1,129 @@
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
+
+// *** TO BE CUSTOMISED ***
+
+var style_cookie_name = "style" ;
+var style_cookie_duration = 30 ;
+var style_domain = "thesitewizard.com" ;
+
+// *** END OF CUSTOMISABLE SECTION ***
+// You do not need to customise anything below this line
+
+function switch_style ( css_title )
+{
+// You may use this script on your site free of charge provided
+// you do not remove this notice or the URL below. Script from
+// https://www.thesitewizard.com/javascripts/change-style-sheets.shtml
+  var i, link_tag ;
+  for (i = 0, link_tag = document.getElementsByTagName("link") ;
+    i < link_tag.length ; i++ ) {
+    if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&
+      link_tag[i].title) {
+      link_tag[i].disabled = true ;
+      if (link_tag[i].title == css_title) {
+        link_tag[i].disabled = false ;
+      }
+    }
+    set_cookie( style_cookie_name, css_title,
+      style_cookie_duration, style_domain );
+  }
+}
+function set_style_from_cookie()
+{
+  var css_title = get_cookie( style_cookie_name );
+  if (css_title.length) {
+    switch_style( css_title );
+  }
+}
+function set_cookie ( cookie_name, cookie_value,
+    lifespan_in_days, valid_domain )
+{
+    // https://www.thesitewizard.com/javascripts/cookies.shtml
+    var domain_string = valid_domain ?
+                       ("; domain=" + valid_domain) : '' ;
+    document.cookie = cookie_name +
+                       "=" + encodeURIComponent( cookie_value ) +
+                       "; max-age=" + 60 * 60 *
+                       24 * lifespan_in_days +
+                       "; path=/" + domain_string ;
+}
+function get_cookie ( cookie_name )
+{
+    // https://www.thesitewizard.com/javascripts/cookies.shtml
+    var cookie_string = document.cookie ;
+    if (cookie_string.length != 0) {
+        var cookie_value = cookie_string.match (
+                        '(^|;)[\s]*' +
+                        cookie_name +
+                        '=([^;]*)' );
+        return decodeURIComponent ( cookie_value[2] ) ;
+    }
+    return '' ;
+}
+
+
+function validate(form)
+{var returnValue=true;
+  var emailid=document.frmLogin.email.value;
+  var posat=emailid.indexOf("@"); 
+if(document.getElementById("name").value == ""|| !(isNaN(document.getElementById("name").value)))
+{alert("Please enter your name in correct");
+return false;}  
+else if(document.getElementById("email").value == ""|| posat==0)
+{alert("Please enter email in correct");
+ return false;  }
+else if(document.getElementById("comments").value == "")
+{alert("Text Area can't be left blank ");
+ return false;  }
+else{
+alert("Form has been submitted");
+return true;
+}}
+
+$(document).ready(function(){
+ $("#but").click(function(){
+        var i;
+        var x = $("li").toArray()
+        for (i = 0; i< x.length; i++) {
+            alert(x[i].innerHTML);
+        }
+    });
+});
+
+$('.container').yahooWeather();
+
+
---
+++ readmer.md
@@ -0,0 +1,7 @@
+# Responsive Mobie Web Site
+Responsive mobile web site designed using html, css and javascript.
+# Introduction
+
+This is a simple responsive mobile website designed using html, css and js. In this website, the main focus is on mobile responsiveness.  It will be a nice website for a reference because the site deign includes so many attractive designs. 
+
+
